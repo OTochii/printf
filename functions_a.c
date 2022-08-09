7f1698a332664ba979379a670eb423dc20eb38b9
OTochii <onyemstochi3@gmail.com>
2022-08-09 09:19:37 -0700

    update functions.c...

--- functions_a.c
+++ functions_a.c
@@ -30,8 +30,7 @@ int print_unsigned(va_list types, char buffer[],
 		buffer[i--] = (num % 10) + '0';
 		num /= 10;
 	}
-
-	i++;	
+	i++;
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
